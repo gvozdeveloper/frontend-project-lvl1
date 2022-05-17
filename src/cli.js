22f27819ae88a064e1fbfb0f66c1f9b5bd7a5705
Alexey Gvozdev <gvozdeveloper@yandex.ru>
2022-05-17 13:58:10 +0300

    Добавил вызов userName в конце

--- src/cli.js
+++ src/cli.js
@@ -3,11 +3,11 @@ import readlineSync from 'readline-sync';
 const runGreetings = () => {
   console.log('Welcome to mind games!');
 
-  const userName = readlineSync.question('May I know your name? -> ');
+  const userName = readlineSync.question('May I have your name? -> ');
 
   console.log(`Hello, ${userName}!`);
   
-  //return userName;
+  return userName;
 };
 
 //console.log(runGreetings());
